0daa5caf291a356a071ce43cd9a06aead06bc403
shinnberry <m-10288496@moe-dl.edu.my>
2024-08-26 16:19:42 +0800

    Update game.js

--- game.js
+++ game.js
@@ -1,40 +1,40 @@
-import { update as updateSnake, draw as drawSnake, SNAKE_SPEED,
-getSnakeHead, snakeIntersection } from './snake.js'
-import { update as updateFood, draw as drawFood } from './food.js
+import { update as updateSnake, draw as drawSnake, SNAKE_SPEED, getSnakeHead, snakeIntersection } from './snake.js'
+import { update as updateFood, draw as drawFood } from './food.js'
 import { outsideGrid } from './grid.js'
 
-let lastRenderTime = 0 
+let lastRenderTime = 0
 let gameOver = false
-const gameBoard = document.getElementByID('game-board')
+const gameBoard = document.getElementById('game-board')
 
-function main (currentTime) {
+function main(currentTime) {
   if (gameOver) {
     if (confirm('You lost. Press ok to restart.')) {
       window.location = '/'
-    } 
-    return 
-  }  
+    }
+    return
+  }
+
 
   window.requestAnimationFrame(main)
-  const secondsSinceLastRender = (currentTime - lastRenderTime) / 
-1000
-  if (secondsSinceLastRender < 1 / SNAKE_SPEED) return 
+  const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000
+  if (secondsSinceLastRender < 1 / SNAKE_SPEED) return
 
-  lastRenderTime = currentTime 
 
-  update() 
+  lastRenderTime = currentTime
+
+  update()
   draw()
 }
 
-window.requestAnimationFrame(main) 
+window.requestAnimationFrame(main)
 
 function update() {
   updateSnake()
   updateFood()
   checkDeath()
-}  
+}
 
-function draw() { 
+function draw() {
   gameBoard.innerHTML = ''
   drawSnake(gameBoard)
   drawFood(gameBoard)
@@ -43,5 +43,3 @@ function draw() {
 function checkDeath() {
   gameOver = outsideGrid(getSnakeHead()) || snakeIntersection()
 }
-  
-  
